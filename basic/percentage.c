f90f1ec81d9deb89c680122f3d6aae47ba56ccab
AmritOnline <amritraj06012006@gmail.com>
2025-05-19 14:44:31 +0530

    added 3 old files

---
+++ basic/percentage.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float m1 = 90; //marks maths
+    float m2 = 91; // physics marks
+    float m3 = 92; //chemestry marks 
+    float m4 = 93; //english marks
+    float m5 = 94; //economics marks
+    float p = (m1+m2+m3+m4+m5)/5;
+    printf("percentage of 5 subjects is : %f",p);
+    return 0;
+      
+} 
