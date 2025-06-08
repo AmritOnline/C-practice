28a24aa7f48777ff076169014b2025610af3b106
AmritOnline <amritraj06012006@gmail.com>
2025-06-08 20:06:38 +0530

    pointer

--- pointer/pointer01.c
+++ pointer/pointer01.c
@@ -1,16 +1,16 @@
 //write a c program to print the address and the value of a variable using pointer.
 
-#include<stdio.h.
+#include<stdio.h>
 int main(){
-	int num=10;
+	int num = 10;
 	int *ptr;
 	
-	ptr=&num;
+	ptr = &num;
 	
-	printf("value of num  :%d\n",num);
-	printf("address of num :%d\n",&num);
-	printf("value using ptr :%d\n",*ptr);
-	printf("address using ptr :%d\n,ptr);
+	printf("Value of num      :%d\n", num);
+	printf("Address of num    :%p\n", &num);
+	printf("Value using ptr   :%d\n", *ptr);
+	printf("Address using ptr :%p\n", ptr);
 	
 	return 0;
 }
