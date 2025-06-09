17f04fdc78083a9ff26881de73bfafa95af293c0
AmritOnline <amritraj06012006@gmail.com>
2025-06-09 14:03:10 +0530

    Q4

---
+++ Functions/function04.c
@@ -0,0 +1,19 @@
+//Write a function to swap two integers using pointers.
+#include<stdio.h>
+
+void swap(int *a, int *b);
+
+int main(){
+	int x=5,y=10;
+	
+	printf("Before swap: x=%d, y=%d\n",x,y);
+	swap(&x,&y);
+	printf("After swap: x= %d, y= %d\n",x,y);
+	return 0;
+}
+void swap(int *a, int *b){
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+}
